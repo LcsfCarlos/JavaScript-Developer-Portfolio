5d52bbc7cd245015ab2c90c99c8da01c8fbdb2e9
Luiz Carlos <luizcarlos-_-@gmail.com>
2025-04-17 10:43:04 +0100

    Finalizando experiencias

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,7 +61,7 @@ function updatePortfolio(profileData) {
 
 function updateProfessionalExperience(profileData) {
   const professionalExperience = document.getElementById(
-    "professional.profileExperience"
+    "profile.professionalExperience"
   );
   professionalExperience.innerHTML = profileData.professionalExperience
     .map((experience) => {
@@ -70,6 +70,7 @@ function updateProfessionalExperience(profileData) {
           <h3 class="title">${experience.name}</h3>
           <p class="period">${experience.period}</p>
           <p>${experience.description}</p>
+          <p class="certificate"><a href="https://web.dio.me/certificates">Certificates</a></p>
         </li>
     `;
     })
