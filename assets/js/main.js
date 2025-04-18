24cb8e53d699ac3e23ad85b491bbb492cfcb29ff
Luiz Carlos <luizcarlos-_-@gmail.com>
2025-04-18 08:24:24 +0100

    corrigindo certificados

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,6 +70,7 @@ function updateProfessionalExperience(profileData) {
           <h3 class="title">${experience.name}</h3>
           <p class="period">${experience.period}</p>
           <p>${experience.description}</p>
+          <p class="certificate"><a href="https://web.dio.me/certificates">Certificates</a></p>
         </li>
     `;
     })
